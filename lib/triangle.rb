705f8dc698f524a17d5671f346afd800a725c832
kylemellander <kylemellander@users.noreply.github.com>
2015-08-09 09:41:19 -0700

    Split width_1 and width_2 methods

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,10 +8,11 @@ class Triangle
     2 * area() / @side_b
   end
   
-  def width
-    base_widths = []
-    width_1 = @side_b - (sqrt((@side_a ^ 2) - (height() ^ 2))
-    base_widths << width_1
-    base_widths << (@side_b - width_1)
+  def width_1
+    @side_b - (sqrt((@side_a ^ 2) - (height() ^ 2))
+  end
+  
+  def width_2
+    @side_b - width_1
   end
 end
