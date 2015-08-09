937483c507f41e1feb5839edde527c6ed95be97b
kylemellander <kylemellander@users.noreply.github.com>
2015-08-09 09:28:54 -0700

    Create width method

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,4 +7,11 @@ class Triangle
   def height
     2 * area() / @side_b
   end
+  
+  def width
+    base_widths = []
+    width_1 = @side_b - (sqrt((@side_a ^ 2) - (height() ^ 2))
+    base_widths << width_1
+    base_widths << (@side_b - width_1)
+  end
 end
